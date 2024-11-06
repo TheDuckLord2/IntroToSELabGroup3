8b8d0b1ad10905266f8e00fb77a51496397f51e3
Ashtanyrein <nyrein@gmail.com>
2024-11-06 13:20:49 -0600

    Added seller to each stock item

--- ecommerce_store/store/models.py
+++ ecommerce_store/store/models.py
@@ -6,12 +6,14 @@ class StoreStock(models.Model):
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock_quantity = models.IntegerField()
+    seller = models.ForeignKey('User', on_delete=models.CASCADE, limit_choices_to={'account_type': 'seller'})
+
 
     class Meta:
         db_table = 'StoreStock'
 
     def __str__(self):
-        return self.name
+        return f"{self.name} by {self.seller.username}"
 
 class User(AbstractUser):
     USER_TYPES = [
