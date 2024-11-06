feb1587af5f4cec09233b73c245c37d294f904b5
Shale951 <166852894+Shale951@users.noreply.github.com>
2024-11-06 14:03:12 -0600

    Update urls.py
    Adds product view to urls to allow products to be looked at,

--- IntroToSELabGroup3-Sam-Branch/ecommerce_store/backendAPI/api/urls.py
+++ IntroToSELabGroup3-Sam-Branch/ecommerce_store/backendAPI/api/urls.py
@@ -4,6 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .models import Cart
 from .views import UserCreationView
 from .views import CartView
+from .views import ProductViewSet
 from .views import StaticDataView
 from .views import RegisterView
 from .views import gethome
@@ -12,6 +13,7 @@ from . import views, admin
 router = DefaultRouter()
 router.register(r'users', UserCreationView) #this is sign up
 router.register(r'cart', CartView) #this is cart
+router.register(r'products', ProductViewSet) #Products view
 router.register(r'register', RegisterView) #this is login
 
 urlpatterns = [
