0c52c03112a08eadcd33b7cc011de1cc7aa98fb8
Shale951 <166852894+Shale951@users.noreply.github.com>
2024-11-09 15:04:39 -0600

    Update models.py
    Added product class

--- IntroToSELabGroup3-Sam-Branch/ecommerce_store/backendAPI/api/models.py
+++ IntroToSELabGroup3-Sam-Branch/ecommerce_store/backendAPI/api/models.py
@@ -32,6 +32,15 @@ class CreatedUser(models.Model):
     class Meta:
         db_table = 'created_users'
 
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock_quantity = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 class Cart(models.Model):
     id = models.CharField(max_length=5, primary_key=True)
