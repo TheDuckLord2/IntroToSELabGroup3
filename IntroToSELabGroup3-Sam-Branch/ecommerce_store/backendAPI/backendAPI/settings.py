589b8bd0cf4a18906a9e3700978f1c099f4bc45a
Shale951 <166852894+Shale951@users.noreply.github.com>
2024-11-09 14:05:51 -0600

    Update settings.py
    Updates settings to allow admin creation

--- IntroToSELabGroup3-Sam-Branch/ecommerce_store/backendAPI/backendAPI/settings.py
+++ IntroToSELabGroup3-Sam-Branch/ecommerce_store/backendAPI/backendAPI/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_extensions',
     #needed to display SQL data through endpoints
     'rest_framework',
+    'templates/store',
 ]
 
 MIDDLEWARE = [
@@ -133,3 +134,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'templates/store.UserCreation'
