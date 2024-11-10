2f808e73a29c5d0222264c34249418d8485f6bb3
samstudentacc <stm303@msstate.edu>
2024-11-10 16:19:48 -0600

    Delete IntroToSELabGroup3-Sam-Branch directory

--- IntroToSELabGroup3-Sam-Branch/ecommerce_store/backendAPI/api/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserCreation, CreatedUser, Cart
-
-# Register your models here.
-
-admin.site.register(UserCreation)
-admin.site.register(CreatedUser)
-admin.site.register(Cart)
